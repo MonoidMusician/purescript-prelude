e26c56072f7452f55cbbf84e676a5e3343806abe
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-04-10 20:35:10 +0100

    Add showString

--- src/Data/Show.erl
+++ src/Data/Show.erl
@@ -1,45 +1,40 @@
 % module Data.Show
 -module(data_show@foreign).
--export([showIntImpl/1, showNumberImpl/1, showCharImpl/1]).
+-export([showIntImpl/1, showNumberImpl/1, showCharImpl/1, showStringImpl/1]).
 
 showIntImpl(N) -> integer_to_list(N).
 showNumberImpl(N) -> float_to_list(N).
 showCharImpl(C) ->
   case C of
-    $b -> "'\\b'";
-    $f -> "'\\f'";
-    $n -> "'\\n'";
-    $r -> "'\\r'";
-    $t -> "'\\t'";
-    $v -> "'\\v'";
+    $\b -> "'\\b'";
+    $\f -> "'\\f'";
+    $\n -> "'\\n'";
+    $\r -> "'\\r'";
+    $\t -> "'\\t'";
+    $\v -> "'\\v'";
     N when N < 16#20; N == 16#7F -> "'\\" ++ integer_to_list(N) ++ "'";
     C when C == $\'; C == $\\ -> "'\\" ++ [C] ++ "'";
     C -> [$\', C, $\']
   end.
 
-% exports.showStringImpl = function (s) {
-%   var l = s.length;
-%   return "\"" + s.replace(
-%     /[\0-\x1F\x7F"\\]/g,
-%     function (c, i) { // jshint ignore:line
-%       switch (c) {
-%         case "\"":
-%         case "\\":
-%           return "\\" + c;
-%         case "\x07": return "\\a";
-%         case "\b": return "\\b";
-%         case "\f": return "\\f";
-%         case "\n": return "\\n";
-%         case "\r": return "\\r";
-%         case "\t": return "\\t";
-%         case "\v": return "\\v";
-%       }
-%       var k = i + 1;
-%       var empty = k < l && s[k] >= "0" && s[k] <= "9" ? "\\&" : "";
-%       return "\\" + c.charCodeAt(0).toString(10) + empty;
-%     }
-%   ) + "\"";
-% };
+showStringImpl(S) ->
+  "\"" ++
+  lists:flatmap(fun(C) ->
+    case C of
+      $\b -> "\\b";
+      $\f -> "\\f";
+      $\n -> "\\n";
+      $\r -> "\\r";
+      $\t -> "\\t";
+      $\v -> "\\v";
+      N when N < 16#20; N == 16#7F -> "\\" ++ integer_to_list(N);
+      _ when C == $\'; C == $\\ -> "\\" ++ [C];
+      _ -> [C]
+    end
+  end, S)
+  ++ "\""
+  .
+
 %
 % showArrayImpl(F) -> fun (XS) ->
 %
