c5ed49a9103f166988ffda2b55cbfbc5ea9413f4
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-05-08 19:52:40 +0100

    Array erl implementations for apply, bind

--- src/Control/Apply.erl
+++ src/Control/Apply.erl
@@ -1,4 +1,22 @@
 % module Control.Apply
 -module(control_apply@foreign).
+-export([arrayApply/1]).
 
-% arrayApply
+arrayApply(Fs) ->
+  fun (Xs) ->
+    Arr = array:new(array:size(Fs)*array:size(Xs)),
+    {_, Result} = array:foldl(
+      fun (_, F, AccN) ->
+        array:foldl(
+          fun (_, X, {N, Acc1}) ->
+            {N+1, array:set(N, F(X), Acc1)}
+          end,
+          AccN,
+          Xs
+        )
+      end,
+      {0, Arr},
+      Fs
+    ),
+    Result
+  end.
--- src/Control/Bind.erl
+++ src/Control/Bind.erl
@@ -1,4 +1,8 @@
 % module Control.Bind
 -module(control_bind@foreign).
+-export([arrayBind/1]).
 
-% arrayBind
+arrayBind(Arr) ->
+  fun (F) ->
+    array:map(fun (_, X) -> F(X) end, Arr)
+  end.
