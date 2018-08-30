c0798ac5a613209c04ef866d5653dd0510f365e5
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2018-08-30 15:40:41 +0000

    Remove unused FFI causing warning

--- src/Data/Bounded.erl
+++ src/Data/Bounded.erl
@@ -1,7 +1,5 @@
 -module(data_bounded@foreign).
--export([topChar/0, bottomChar/0, refEq/2, refIneq/2]).
+-export([topChar/0, bottomChar/0]).
 
 topChar() -> 16#10FFFF.
 bottomChar() -> 0.
-refEq(A, B) -> A =:= B.
-refIneq(A, B) -> A =/= B.
